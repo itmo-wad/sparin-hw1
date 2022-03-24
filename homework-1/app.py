bfaa05d7889563cd6db6c6471bb3d4b0dd3f62b0
Medveditskov Yuriy Sergeevich <sparin285@gmail.com>
2022-03-24 14:49:44 +0300

    Add advanced part of homework 1

--- homework-1/app.py
+++ homework-1/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return app.send_static_file('index.html')
+@app.route('/', defaults={'full_name': 'Medveditskov Yuriy Sergeevich'})
+@app.route('/<string:full_name>')
+def index(full_name):
+    return render_template("index.html", title="Poo CV", full_name=full_name)
+
+@app.route("/profile")
+def profile():
+    return redirect("/")
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
